64d777892a913c122e72e40728785f5982496944
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-22 13:39:53 -0700

    Generate a Simplified Root Cert in Memory
    This commit utilizes some classes including `X509`, `X509Builder`, `X509Name`,
    and `X509NameBuilder` to set some preliminary values into a name and a
    certificate and generate it. This is not expected to be usable at the
    moment. It is more for exploring the available components within the
    `OpenSSL` library.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
+use openssl::x509::X509;
+use openssl::x509::X509Name;
+use openssl::x509::X509NameBuilder;
+use openssl::x509::X509Builder;
+
+fn main() -> Result<(), std::io::Error> {
+    let mut name_builder: X509NameBuilder = X509NameBuilder::new()?;
+    name_builder.append_entry_by_text("C", "US")?;
+    name_builder.append_entry_by_text("ST", "CA")?;
+    name_builder.append_entry_by_text("O", "Rymeon")?;
+    name_builder.append_entry_by_text("CN", "Rymeon CA Root")?;
+    let name: X509Name = name_builder.build();
+
+    let mut builder = X509Builder::new()?;
+    builder.set_issuer_name(&name)?;
+    builder.set_subject_name(&name)?;
+
+    let cert: X509 = builder.build();
+    println!("{:?}", cert);
+
     println!("Hello, world!");
+    Ok(())
 }
