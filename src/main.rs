6e96cc42fa055fd66db753f3df8ba2177af3ab6f
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-24 16:52:47 -0700

    Certificate: Start Function Extraction
    This commit does a small bit of refactoring to extract a
    `build_root_name` and a `build_serial_number` function, respectively.
    Very crude and still a long way to go.

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,24 @@ use openssl::x509::X509NameBuilder;
 use openssl::x509::X509Builder;
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn build_root_name() -> Result<X509Name, Box<dyn Error>> {
     let mut name_builder: X509NameBuilder = X509NameBuilder::new()?;
     name_builder.append_entry_by_text("C", "US")?;
     name_builder.append_entry_by_text("ST", "CA")?;
     name_builder.append_entry_by_text("O", "Rymeon")?;
     name_builder.append_entry_by_text("CN", "Rymeon CA Root")?;
     let name: X509Name = name_builder.build();
+    Ok(name)
+}
+
+fn build_serial_number(serial: &str) -> Result<Asn1Integer, Box<dyn Error>> {
+    let bn_serial = BigNum::from_hex_str(serial)?;
+    let asn1_serial = Asn1Integer::from_bn(&bn_serial)?;
+    Ok(asn1_serial)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let name = build_root_name()?;
 
     let mut builder = X509Builder::new()?;
     builder.set_issuer_name(&name)?;
@@ -24,9 +35,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     builder.set_not_before(not_before.as_ref())?;
     builder.set_not_after(not_after.as_ref())?;
 
-    let bn_serial = BigNum::from_hex_str("1234567890abcdef1234567890abcdef")?;
-    let asn1_serial = Asn1Integer::from_bn(&bn_serial)?;
-    builder.set_serial_number(&asn1_serial)?;
+    let serial = build_serial_number("1234567890abcdef1234567890abcdef")?;
+    builder.set_serial_number(&serial)?;
 
     let cert: X509 = builder.build();
     println!("{:?}", cert);
