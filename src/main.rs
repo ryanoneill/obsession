d25d7809f1c69aabb3b5b338f279322f816eb762
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-26 11:17:52 -0700

    Library: Added `ToHashMap` Trait and Implementation
    This commit adds a `ToHashMap` trait and an implementation for
    `X509Name`. This trait puts the values for an `X509Name` into a
    `HashMap<String, String>` in order to make verification of values within
    an `X509Name` much more straightforward.

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,36 @@ use openssl::x509::X509;
 use openssl::x509::X509Name;
 use openssl::x509::X509NameBuilder;
 use openssl::x509::X509Builder;
+use std::collections::HashMap;
 use std::error::Error;
+
+#[allow(dead_code)]
+trait ToHashMap<A, B> {
+    fn to_hash_map(&self) -> HashMap<A, B>;
+}
+
+impl ToHashMap<String, String> for X509Name {
+    fn to_hash_map(&self) -> HashMap<String, String> {
+        let mut result = HashMap::new();
+
+        for entry in self.entries() {
+            let key = entry.object().to_string();
+            let value = String::from_utf8_lossy(
+                entry.data().as_slice()
+            ).to_string();
+            result.insert(key, value);
+        }
+
+        result
+    }
+}
 
 fn build_root_name() -> Result<X509Name, Box<dyn Error>> {
     let mut name_builder: X509NameBuilder = X509NameBuilder::new()?;
     name_builder.append_entry_by_text("C", "US")?;
     name_builder.append_entry_by_text("ST", "CA")?;
     name_builder.append_entry_by_text("O", "Rymeon")?;
-    name_builder.append_entry_by_text("CN", "Rymeon CA Root")?;
+    name_builder.append_entry_by_text("CN", "Rymeon Root CA")?;
     let name: X509Name = name_builder.build();
     Ok(name)
 }
@@ -46,7 +68,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 #[cfg(test)]
 mod tests {
-    use crate::build_root_name;
+    use crate::{build_root_name, ToHashMap};
 
     #[test]
     fn build_root_name_is_ok() {
@@ -58,6 +80,22 @@ mod tests {
     fn build_root_name_has_4_entries() {
         let root_name = build_root_name().unwrap();
         assert_eq!(root_name.entries().count(), 4);
+    }
+
+    #[test]
+    fn build_root_name_hashmap() {
+        let root_name = build_root_name().unwrap();
+        let map = root_name.to_hash_map();
+        println!("{:?}", map);
+        assert_eq!(map.len(), 4);
+        assert!(map.contains_key("countryName"));
+        assert_eq!(map["countryName"], "US");
+        assert!(map.contains_key("stateOrProvinceName"));
+        assert_eq!(map["stateOrProvinceName"], "CA");
+        assert!(map.contains_key("organizationName"));
+        assert_eq!(map["organizationName"], "Rymeon");
+        assert!(map.contains_key("commonName"));
+        assert_eq!(map["commonName"], "Rymeon Root CA");
     }
 
 }
